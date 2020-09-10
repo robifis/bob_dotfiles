1cba2556959a2bb8625db0141ddb11282c212948
Bobby <bobby@olejnik.dev>
2020-09-10 12:57:02 +0100

    Automated git commit

--- dotfileBackup.sh
+++ dotfileBackup.sh
@@ -2,4 +2,4 @@
 
 sudo cp -r ~/.vimrc ~/.zshrc ~/.xinitrc ~/.Xauthority ~/.Xresources ~/bob_dotfiles
 sudo cp -r ~/.config/alacritty ~/.config/i3 ~/.config/rofi ~/.config/polybar ~/bob_dotfiles
-./automatedGit
+./automatedGit.sh
